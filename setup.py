b37ecbeced29037785bbbbdf431e404765393070
Maxim Syabro <syabro@gmail.com>
2012-09-22 22:41:48 +0400

    Added setup.py and soupselect.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='soupselect',
+    version='',
+    packages=[''],
+    url='https://github.com/syabro/soupselect',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
